8fa415fa4c16689d99febae7c96778d181a5ac65
Praveen Sentha <praveen.sentha@gmail.com>
2026-08-16 20:46:22 -0700

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0 
+        right = 1
+        total = 0
+        while right < len(prices):
+            if prices[right] < prices[left]:
+                left += 1
+            else:
+                total = max(total, prices[right] - prices[left])
+                right += 1
+        return total
+        
